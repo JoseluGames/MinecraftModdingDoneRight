9def033d1afe012acdfe73bea44e8ed834318e28
JoseluGames <joselugamesofficial@gmail.com>
2016-06-27 23:42:08 +0200

    Add commentary to the constants class

--- src/main/java/com/jlgm/mmdr/lib/MMDRConstants.java
+++ src/main/java/com/jlgm/mmdr/lib/MMDRConstants.java
@@ -1,11 +1,16 @@
 package com.jlgm.mmdr.lib;
 
 public class MMDRConstants {
+	//The name "behind the scenes"
 	public static final String MODID = "jlgm_MMDR";
+	//The public name
 	public static final String NAME = "Minecraft Modding Done Right";
+	//The version of the mod
 	public static final String VERSION = "0.1a";
+	//The Minecraft version this mod is focused to work with
 	public static final String ACCEPTEDMINECRAFTVERSIONS = "[1.10]";
 	
+	//The package route of the proxys
 	public static final String CLIENT_PROXY = "com.jlgm.mmdr.main.ClientProxy";
 	public static final String SERVER_PROXY = "com.jlgm.mmdr.main.ServerProxy";
 }
