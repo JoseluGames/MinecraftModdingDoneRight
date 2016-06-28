278602ce30c9ba7e9095fcf0222db1a9849a718f
JoseluGames <joselugamesofficial@gmail.com>
2016-06-28 14:20:44 +0200

    Add comments to the Creative Tabs part

--- src/main/java/com/jlgm/mmdr/creativetab/CreativeTabMMDR.java
+++ src/main/java/com/jlgm/mmdr/creativetab/CreativeTabMMDR.java
@@ -9,7 +9,8 @@ public class CreativeTabMMDR extends CreativeTabs {
 	public CreativeTabMMDR(int index, String label) {
 		super(index, label);
 	}
-
+	
+	//Sets the item to show in the tab.
 	@Override
 	public Item getTabIconItem() {
 		return Items.DIAMOND_PICKAXE;
--- src/main/java/com/jlgm/mmdr/creativetab/MMDRCreativeTab.java
+++ src/main/java/com/jlgm/mmdr/creativetab/MMDRCreativeTab.java
@@ -5,12 +5,15 @@ import net.minecraftforge.common.config.Configuration;
 
 public class MMDRCreativeTab {
 	
+	//Create the variable for the tab
 	public static CreativeTabs tabMMDR;
 	
 	public static void mainRegistry(Configuration par1Configuration){
 		initialiseCreativeTab();
 	}
 	
+	//Set a value to the variable (In this case the value is a creative tab).
+	//The second parameter is the unlocalized name. This is used in the .lang files to translate the name.
 	public static void initialiseCreativeTab(){
 		tabMMDR = new CreativeTabMMDR(CreativeTabs.getNextID(), "MMDR");
 	}
