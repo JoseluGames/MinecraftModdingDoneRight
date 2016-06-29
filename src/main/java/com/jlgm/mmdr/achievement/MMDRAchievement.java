9ac3bc1ae003f6ca91a4c336d99445b59565b796
JoseluGames <joselugamesofficial@gmail.com>
2016-06-29 22:32:09 +0200

    Add achievements page

--- src/main/java/com/jlgm/mmdr/achievement/MMDRAchievement.java
+++ src/main/java/com/jlgm/mmdr/achievement/MMDRAchievement.java
@@ -2,13 +2,11 @@ package com.jlgm.mmdr.achievement;
 
 import com.jlgm.mmdr.block.MMDRBlock;
 
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
-import net.minecraft.item.ItemStack;
 import net.minecraft.stats.Achievement;
 import net.minecraft.stats.AchievementList;
-import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.AchievementPage;
 
 public class MMDRAchievement {
 	
@@ -16,10 +14,21 @@ public class MMDRAchievement {
 	public static Achievement example_achievement_2;
 	public static Achievement example_achievement_3;
 	
+	public static Achievement example_achievement_4;
+	public static Achievement example_achievement_5;
+	
+	public static AchievementPage example_achievement_page_1;
+	
 	public static void registerAchievement(){
 		System.out.println("Test");
 		example_achievement_1 = new Achievement("achievement.example_achievement_1", "example_achievement_1", -2, 3, MMDRBlock.example_block_1, AchievementList.OPEN_INVENTORY).registerStat();
 		example_achievement_2 = new Achievement("achievement.example_achievement_2", "example_achievement_2", -2, 5, MMDRBlock.example_static_lamp_1, MMDRAchievement.example_achievement_1).registerStat();
 		example_achievement_3 = new Achievement("achievement.example_achievement_3", "example_achievement_3", 0, 7, Items.NETHER_STAR, MMDRAchievement.example_achievement_2).setSpecial().registerStat();
+		
+		example_achievement_4 = new Achievement("achievement.example_achievement_4", "example_achievement_4", 0, 0, Blocks.TNT, null).registerStat();
+		example_achievement_5 = new Achievement("achievement.example_achievement_5", "example_achievement_5", 2, 2, Items.SKULL, MMDRAchievement.example_achievement_4).setSpecial().registerStat();
+		
+		example_achievement_page_1 = new AchievementPage("Example Achievement Page 1", example_achievement_4, example_achievement_5);
+		AchievementPage.registerAchievementPage(example_achievement_page_1);
 	}
 }
--- src/main/java/com/jlgm/mmdr/event/MMDREventListener.java
+++ src/main/java/com/jlgm/mmdr/event/MMDREventListener.java
@@ -4,13 +4,18 @@ import com.jlgm.mmdr.achievement.MMDRAchievement;
 import com.jlgm.mmdr.block.MMDRBlock;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.item.ItemTossEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
 import net.minecraftforge.event.world.BlockEvent.PlaceEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class MMDREventListener {
+	
+	public static EntityPlayer playerDetonator;
 	
 	@SubscribeEvent
 	public void onEventPlaceExampleBlock(PlaceEvent event){
@@ -27,6 +32,28 @@ public class MMDREventListener {
 	public void onEventDropItem(ItemTossEvent event){
 		if(event.getEntityItem().getEntityItem().getItem().equals(Item.getItemFromBlock(MMDRBlock.example_block_1))){
 			event.getPlayer().addStat(MMDRAchievement.example_achievement_3);
+		}
+	}
+	
+	@SubscribeEvent
+	public void onEventRightClickBlock(RightClickBlock event){
+		if(event.getItemStack() != null && event.getWorld().getBlockState(event.getPos()).getBlock() != null){
+			if(event.getWorld().getBlockState(event.getPos()).getBlock().equals(Blocks.TNT)
+					&& event.getItemStack().getItem().equals(Items.FLINT_AND_STEEL)
+					|| event.getItemStack().getItem().equals(Items.FIRE_CHARGE)){
+				playerDetonator = event.getEntityPlayer();
+				event.getEntityPlayer().addStat(MMDRAchievement.example_achievement_4);
+			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void onEventPlayerDeath(LivingDeathEvent event){
+		if(event.getEntityLiving() != null && event.getEntityLiving() instanceof EntityPlayer){
+			EntityPlayer player = (EntityPlayer) event.getEntityLiving();
+			if(player.isEntityEqual(playerDetonator)){
+				player.addStat(MMDRAchievement.example_achievement_5);
+			}
 		}
 	}
 }
