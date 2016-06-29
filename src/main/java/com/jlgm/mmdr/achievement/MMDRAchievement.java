bd88aed2d36523e5b62bdd2a0bad220eb9151f3c
JoseluGames <joselugamesofficial@gmail.com>
2016-06-29 22:56:17 +0200

    Add modid to achievements id to be more compatible with other mods

--- src/main/java/com/jlgm/mmdr/achievement/MMDRAchievement.java
+++ src/main/java/com/jlgm/mmdr/achievement/MMDRAchievement.java
@@ -1,6 +1,7 @@
 package com.jlgm.mmdr.achievement;
 
 import com.jlgm.mmdr.block.MMDRBlock;
+import com.jlgm.mmdr.lib.MMDRConstants;
 
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -20,12 +21,12 @@ public class MMDRAchievement {
 	public static AchievementPage example_achievement_page_1;
 	
 	public static void registerAchievement(){
-		example_achievement_1 = new Achievement("achievement.example_achievement_1", "example_achievement_1", -2, 3, MMDRBlock.example_block_1, AchievementList.OPEN_INVENTORY).registerStat();
-		example_achievement_2 = new Achievement("achievement.example_achievement_2", "example_achievement_2", -2, 5, MMDRBlock.example_static_lamp_1, MMDRAchievement.example_achievement_1).registerStat();
-		example_achievement_3 = new Achievement("achievement.example_achievement_3", "example_achievement_3", 0, 7, Items.NETHER_STAR, MMDRAchievement.example_achievement_2).setSpecial().registerStat();
+		example_achievement_1 = new Achievement(MMDRConstants.MODID + "." + "achievement.example_achievement_1", "example_achievement_1", -2, 3, MMDRBlock.example_block_1, AchievementList.OPEN_INVENTORY).registerStat();
+		example_achievement_2 = new Achievement(MMDRConstants.MODID + "." + "achievement.example_achievement_2", "example_achievement_2", -2, 5, MMDRBlock.example_static_lamp_1, MMDRAchievement.example_achievement_1).registerStat();
+		example_achievement_3 = new Achievement(MMDRConstants.MODID + "." + "achievement.example_achievement_3", "example_achievement_3", 0, 7, Items.NETHER_STAR, MMDRAchievement.example_achievement_2).setSpecial().registerStat();
 		
-		example_achievement_4 = new Achievement("achievement.example_achievement_4", "example_achievement_4", 0, 0, Blocks.TNT, null).registerStat();
-		example_achievement_5 = new Achievement("achievement.example_achievement_5", "example_achievement_5", 2, 2, Items.SKULL, MMDRAchievement.example_achievement_4).setSpecial().registerStat();
+		example_achievement_4 = new Achievement(MMDRConstants.MODID + "." + "achievement.example_achievement_4", "example_achievement_4", 0, 0, Blocks.TNT, null).registerStat();
+		example_achievement_5 = new Achievement(MMDRConstants.MODID + "." + "achievement.example_achievement_5", "example_achievement_5", 2, 2, Items.SKULL, MMDRAchievement.example_achievement_4).setSpecial().registerStat();
 		
 		example_achievement_page_1 = new AchievementPage("Example Achievement Page 1", example_achievement_4, example_achievement_5);
 		AchievementPage.registerAchievementPage(example_achievement_page_1);
