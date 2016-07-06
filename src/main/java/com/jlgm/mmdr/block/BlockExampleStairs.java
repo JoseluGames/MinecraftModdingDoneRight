90e4c6232c64079be40f2b4d9db107c7f300369e
JoseluGames <joselugamesofficial@gmail.com>
2016-07-06 15:11:58 +0200

    Add example stair

---
+++ src/main/java/com/jlgm/mmdr/block/BlockExampleStairs.java
@@ -0,0 +1,12 @@
+package com.jlgm.mmdr.block;
+
+import net.minecraft.block.BlockStairs;
+import net.minecraft.block.state.IBlockState;
+
+public class BlockExampleStairs extends BlockStairs{
+
+	protected BlockExampleStairs(IBlockState modelState) {
+		super(modelState);
+	}
+
+}
--- src/main/java/com/jlgm/mmdr/block/MMDRBlock.java
+++ src/main/java/com/jlgm/mmdr/block/MMDRBlock.java
@@ -9,13 +9,10 @@ import com.jlgm.mmdr.creativetab.MMDRCreativeTab;
 import com.jlgm.mmdr.lib.MMDRConstants;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockWall;
-import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderItem;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemBlock;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -27,6 +24,9 @@ public class MMDRBlock {
 	
 	public static Block example_static_lamp_1;
 	public static ItemBlock example_static_lamp_1_ItemBlock;
+	
+	public static Block example_stair_1;
+	public static ItemBlock example_stair_1_ItemBlock;
 
 	public static Block example_wall_1;
 	public static ItemBlock example_wall_1_ItemBlock;
@@ -42,6 +42,9 @@ public class MMDRBlock {
 		example_static_lamp_1 = new Block(Material.GLASS).setUnlocalizedName("example_static_lamp_1").setHardness(0.3F).setResistance(0.2F).setLightLevel(1F).setCreativeTab(MMDRCreativeTab.example_creative_tab_1);
 		example_static_lamp_1_ItemBlock = new ItemBlock(example_static_lamp_1);
 
+		example_stair_1 = new BlockExampleStairs(MMDRBlock.example_block_1.getDefaultState()).setUnlocalizedName("example_stair_1").setCreativeTab(MMDRCreativeTab.example_creative_tab_1);
+		example_stair_1_ItemBlock = new ItemBlock(example_stair_1);
+
 		example_wall_1 = new BlockExampleWall(MMDRBlock.example_block_1).setUnlocalizedName("example_wall_1").setCreativeTab(MMDRCreativeTab.example_creative_tab_1);
 		example_wall_1_ItemBlock = new ItemBlock(example_wall_1);
 	}
@@ -53,6 +56,9 @@ public class MMDRBlock {
 		GameRegistry.register(example_static_lamp_1.setRegistryName("example_static_lamp_1"));
 		GameRegistry.register(example_static_lamp_1_ItemBlock.setRegistryName(example_static_lamp_1.getRegistryName()));
 		
+		GameRegistry.register(example_stair_1.setRegistryName("example_stair_1"));
+		GameRegistry.register(example_stair_1_ItemBlock.setRegistryName(example_stair_1.getRegistryName()));
+		
 		GameRegistry.register(example_wall_1.setRegistryName("example_wall_1"));
 		GameRegistry.register(example_wall_1_ItemBlock.setRegistryName(example_wall_1.getRegistryName()));
 	}
@@ -63,6 +69,8 @@ public class MMDRBlock {
 		renderItem.getItemModelMesher().register(example_block_1_ItemBlock, 0, new ModelResourceLocation(MMDRConstants.MODID + ":" + "example_block_1", "inventory"));
 		
 		renderItem.getItemModelMesher().register(example_static_lamp_1_ItemBlock, 0, new ModelResourceLocation(MMDRConstants.MODID + ":" + "example_static_lamp_1", "inventory"));
+		
+		renderItem.getItemModelMesher().register(example_stair_1_ItemBlock, 0, new ModelResourceLocation(MMDRConstants.MODID + ":" + "example_stair_1", "inventory"));
 		
 		renderItem.getItemModelMesher().register(example_wall_1_ItemBlock, 0, new ModelResourceLocation(MMDRConstants.MODID + ":" + "example_wall_1", "inventory"));
 	}
